e735db5ac164200da0d324f58b2ba1efab56d41b
gsure-tech <Geeescode@>
2023-02-19 18:52:27 +0100

    after another correction

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ import CountUp from "react-countup";
 import { useInView } from "react-intersection-observer";
 //motion
 
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 // //variant
 // import { fadeIn } from "../variants";
 
@@ -16,39 +17,48 @@ const About = () => {
   return (
     <section className="section" id="about" ref={ref}>
       <div className="container mx-auto">
-        <div className="flex flex-col gap-y-10 lg:flex-row lg-items-center  lg:gap-x-20 lg:gap-y-0 ">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center  lg:gap-x-20 lg:gap-y-0 h-screen ">
           {/* image */}
-          <div
-            className="flex-1 bg-about bg-contain bg-no-repeat h-[450px]
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[600px]
           mix-blend-lighten  bg-top"
-          >
-            
-          </div>
+          ></motion.div>
           {/* text */}
-          <div className="flex-1">
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
             <h2 className="h2 text-accent"> About me.</h2>
-            <h3 className="h3 mb-4">I am a Back-end Developer with over 2years of experience</h3>
+            <h3 className="h3 mb-4">
+              I am a Back-end Developer with over 2years of experience
+            </h3>
             <p className="mb-6">
               You should work with me and work with me and work with me and work
               with me and work with me and work with me and just be working with
               me and be working with me with no going back.
             </p>
             {/* stats */}
-            <div className="flex gaap-x-6 lg:gap-x-10 mb-12">
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
               <div>
                 <div className="text-[40px] font-tetiary text-gradient mb-2">
                   {InView ? <CountUp start={0} end={3} duration={2} /> : null}
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
-                  years of <br />
+                  Years of <br />
                   Experience
                 </div>
               </div>
 
               <div>
                 <div className="text-[40px] font-tetiary text-gradient mb-2">
-                  {InView ? <CountUp start={0} end={7} duration={2} /> : null}
-                  +
+                  {InView ? <CountUp start={0} end={7} duration={2} /> : null}+
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   Projects <br />
@@ -57,8 +67,7 @@ const About = () => {
               </div>
               <div>
                 <div className="text-[40px] font-tetiary text-gradient mb-2">
-                  {InView ? <CountUp start={0} end={15} duration={2} /> : null}
-                  +
+                  {InView ? <CountUp start={0} end={15} duration={2} /> : null}+
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   Satisfied <br />
@@ -69,10 +78,10 @@ const About = () => {
             <div className="flex gap-x-8  items-center">
               <button className="btn btn-lg">Contact me</button>
               <a href="contact.js" className="text-gradient btn-link">
-                My Portfolio 
+                My Portfolio
               </a>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
